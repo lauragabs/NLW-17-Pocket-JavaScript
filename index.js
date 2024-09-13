6de354645224ef65b49ea244d1254adb332d8a84
Laura Gabriely <laura.587523la@gmail.com>
2024-09-13 20:15:03 -0300

    fundamentos001

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-//hello world
-console.log("hello world")
+const start = () => {
+
+    while (true) {
+        let opcao = "sair"
+        switch (opcao){
+            case "cadastar":
+                console.log("Vamos cadastrar")
+                break;
+            case "listar":
+                console.log("Vamos listar")
+                break;
+            case "sair":
+                return
+            
+        }
+    }
+}
