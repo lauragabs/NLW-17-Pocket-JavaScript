42c6581b7ee4b062a107b591f6e63d5d8da90dcd
Laura Gabriely <laura.587523la@gmail.com>
2024-09-13 22:00:40 -0300

    Adicionar Metas e ListarMetas

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 const {select, input} = require('@inquirer/prompts')
 
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
 
+let metas = [ meta ]
 
+const cadastrarMetas = async () => {
+    const meta = await input [{mensage: 'Digite nova meta:'}]
+
+    if (meta.length  == 0) {
+        console.log('Meta não pode ser vazia')
+        return
+    }
+
+    metas.push(
+        { value: meta, checked: false }
+    )
+
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
+
+}
 
 const start = async() => {
 
